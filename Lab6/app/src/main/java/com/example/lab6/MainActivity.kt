cd6fda9133345edbb01d44644fe6d292e05ef7a0
dylan-s-young <dsyoung0731@gmail.com>
2024-04-15 22:30:30 -0400

    changes

--- Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.lab6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab6.ui.theme.Lab6Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +31,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        Greeting("Greetings! Welcome to Radio + Audio App!")
+                        createRow()
+                        createRowVideo()
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun createRow() {
+    Row() {
+        Text(
+            text = "Calvin Harris",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 30.dp)
+        )
+
+        Button(onClick = {
+
+        }) {
+            Text("Play")
+        }
+
+        Button(onClick = {
+
+        }) {
+            Text("Stop")
+        }
+    }
+}
+
+@Composable
+fun createRowVideo() {
+    Row() {
+        Text(
+            text = "Calvin Harris Video",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 30.dp)
+        )
+
+        Button(onClick = {
+
+        }) {
+            Text("Play")
+        }
+
+        Button(onClick = {
+
+        }) {
+            Text("Stop")
         }
     }
 }
@@ -36,6 +95,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
